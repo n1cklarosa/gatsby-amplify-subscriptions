7459371d756974235760b646adc9780640c11fe5
n1cklarosa <nick@nicklarosa.net>
2020-10-01 11:37:34 +1000

    Added front page data

--- src/content/data.js
+++ src/content/data.js
@@ -1,22 +1,93 @@
-
 export const data = {
   dev: {
     packages: [
       {
+        id: "price_1HXFjwAx3RhHxtkSNqd7B70Z",
+        title: "HLS Database Admin",
+        subtitle: "Access and download your station history, anytime, anywhere",
+        items: [
+          "No history limit",
+          "Station access only (no public access)",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+        ],
+        currentPrice: 150,
+        categories: ["tier1"],
+      },
+      {
+        id: "price_1HXFkgAx3RhHxtkSDIkSLIDM",
+        title: "HLS Database - Bronze",
+        subtitle:
+          "Station with moderate usage. For small to medium sized audiences",
+        items: [
+          "6 months worth of station history",
+          "Publicly sharable, embed on your website or mobile app",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+          "10000 hours of Live Listening per month",
+          "Unlimited Ondemand Listening per month",
+          "On air now statistics",
+        ],
+        currentPrice: 250,
+        categories: ["tier1"],
+      },
+      {
+        id: "price_1HXFogAx3RhHxtkSDQQVXtZk",
+        title: "HLS Database - Silver",
+        subtitle:
+          "For small to medium sized audiences that need that little extra grunt behind them",
+        items: [
+          "12 months worth of station history",
+          "Publicly sharable, embed on your website or mobile app",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+          "15000 hours of Live Listening per month",
+          "Unlimited Ondemand Listening per month",
+          "On air now statistics",
+        ],
+        currentPrice: 350,
+        categories: ["tier1"],
+      },
+      {
+        id: "price_1HXFs0Ax3RhHxtkSHi6iomkV",
+        title: "HLS Database - Gold ",
+        subtitle:
+          "For medium to large sized audiences that need that little extra grunt behind them",
+        items: [
+          "36 months worth of station history",
+          "Publicly sharable, embed on your website or mobile app",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+          "15000 hours of Live Listening per month",
+          "Unlimited Ondemand Listening per month",
+          "On air now statistics",
+        ],
+        currentPrice: 500,
+        categories: ["tier1"],
+      },
+      {
         id: "price_1HTzBbAx3RhHxtkSmkVuVTNL",
         title: "Test Subscription",
         subtitle: "Here is a subtitle",
-        items: ["Item number 1", "1 month of history","Item number 1", "1 month of historyt","Item number 1", "1 month of historyt"],
+        items: [
+          "Item number 1",
+          "1 month of history",
+          "Item number 1",
+          "1 month of historyt",
+          "Item number 1",
+          "1 month of historyt",
+        ],
         currentPrice: 10,
-        categories : ["tier1"]
+        categories: ["tier3"],
       },
       {
         id: "price_1HUQu8Ax3RhHxtkSyip5bPu5",
         title: "Silver Subscription",
-        subtitle: "Here is a subtitle that is a little bit longer with some more info",
+        subtitle:
+          "Here is a subtitle that is a little bit longer with some more info",
         items: ["Item number 1", "1 month of historyt"],
         currentPrice: 100,
-        categories : ["tier1"]
+        categories: ["tier3"],
       },
       {
         id: "price_1HTzBbAx3RhHxtkSmkVuVTNL",
@@ -24,15 +95,16 @@ export const data = {
         subtitle: "Here is a subtitle",
         items: ["Item number", "1 month of history"],
         currentPrice: 250,
-        categories : ["tier1"]
+        categories: ["tier3"],
       },
       {
         id: "price_1HUQu8Ax3RhHxtkSyip5bPu5",
         title: "Silver Subscription",
-        subtitle: "Here is a subtitle that is a little bit longer with some more info",
+        subtitle:
+          "Here is a subtitle that is a little bit longer with some more info",
         items: ["Item number 1", "1 month of historyt"],
         currentPrice: 100,
-        categories : ["tier2"]
+        categories: ["tier2"],
       },
       {
         id: "price_1HTzBbAx3RhHxtkSmkVuVTNL",
@@ -40,32 +112,79 @@ export const data = {
         subtitle: "Here is a subtitle",
         items: ["Item number", "1 month of history"],
         currentPrice: 250,
-        categories : ["tier2"]
+        categories: ["tier2"],
       },
     ],
-  }, 
+  },
   prod: {
     packages: [
       {
-        id: "price_1HTzBbAx3RhHxtkSmkVuVTNL",
-        title: "Test Subscription",
-        subtitle: "Here is a subtitle",
-        items: ["Item number 1", "1 month of history"],
-        categories : ["tier1"]
+        id: "price_1HXFjwAx3RhHxtkSNqd7B70Z",
+        title: "HLS Database Admin",
+        subtitle: "Access and download your station history, anytime, anywhere",
+        items: [
+          "No history limit",
+          "SMS and Email notifications",
+          "Station access only (no public access)",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+        ],
+        currentPrice: 150,
+        categories: ["tier1"],
       },
       {
-        id: "price_1HTzBbAx3RhHxtkSmkVuVTNL",
-        title: "Test Subscription",
-        subtitle: "Here is a subtitle",
-        items: ["Item number 1", "1 month of history"],
-        categories : ["tier1"]
+        id: "price_1HXFkgAx3RhHxtkSDIkSLIDM",
+        title: "HLS Database - Bronze",
+        subtitle:
+          "Station with moderate usage. For small to medium sized audiences",
+        items: [
+          "6 months worth of station history",
+          "SMS and Email notifications",
+          "Publicly sharable, embed on your website or mobile app",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+          "10000 hours of Live Listening each month",
+          "Uncapped Ondemand Listening each month",
+          "On air now statistics",
+        ],
+        currentPrice: 250,
+        categories: ["tier1"],
       },
       {
-        id: "price_1HTzBbAx3RhHxtkSmkVuVTNL",
-        title: "Test Subscription",
-        subtitle: "Here is a subtitle",
-        items: ["Item number 1", "1 month of history"],
-        categories : ["tier1"]
+        id: "price_1HXFogAx3RhHxtkSDQQVXtZk",
+        title: "HLS Database - Silver",
+        subtitle:
+          "For small to medium sized audiences that need that little extra grunt behind them",
+        items: [
+          "12 months worth of station history",
+          "SMS and Email notifications",
+          "Publicly sharable, embed on your website or mobile app",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+          "15000 hours of Live Listening each month",
+          "Uncapped Ondemand Listening each month",
+          "On air now statistics",
+        ],
+        currentPrice: 350,
+        categories: ["tier1"],
+      },
+      {
+        id: "price_1HXFs0Ax3RhHxtkSHi6iomkV",
+        title: "HLS Database - Gold",
+        subtitle:
+          "For medium to large sized audiences that need that little extra grunt behind them",
+        items: [
+          "36 months worth of station history",
+          "SMS and Email notifications",
+          "Publicly sharable, embed on your website or mobile app",
+          "Download your audio in aac, mp3 or wav",
+          "Full API access",
+          "Uncapped Live Listening each month",
+          "Uncapped Ondemand Listening each month",
+          "On air now statistics",
+        ],
+        currentPrice: 500,
+        categories: ["tier1"],
       },
     ],
   },
